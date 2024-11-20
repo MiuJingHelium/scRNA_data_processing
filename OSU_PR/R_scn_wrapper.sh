70799c691f2debd9618e91b09f4c5bd788b1897e
Carisa Zeng <zccy2001@hotmail.com>
2024-11-20 14:55:08 -0600

    Update codes for data analysis and change directory names

--- OSU_PR/R_scn_wrapper.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-TOKEN=$1
-mkdir -p ./$TOKEN
-Rscript ./Object_Creation.R ./"Manual_Integrated.Robj" ./$TOKEN
-mkdir -p ./check_objects
-Rscript ./Check_Object.R ./$TOKEN ./check_objects/
-
